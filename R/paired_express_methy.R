503b272d49a5074a6bd6a81cbd4b4c3a77d78117
NWPU-903PR <37072138+NWPU-903PR@users.noreply.github.com>
2021-08-25 20:10:58 +0800

    Update paired_express_methy.R

--- R/paired_express_methy.R
+++ R/paired_express_methy.R
@@ -1,5 +1,5 @@
 ##match gene count and methylation intensity
-match_expr_methy <- function(gene_count_infor, decay_methy,OUTPUT_DIR){
+match_expr_methy <- function(gene_count_infor, gene_methy_infor,OUTPUT_DIR){
   ##select DE gene data
   gene_count <- gene_count_infor[[1]]
   gene_count <- gene_count[which(rowSums(gene_count)>10),]
@@ -19,16 +19,19 @@ match_expr_methy <- function(gene_count_infor, decay_methy,OUTPUT_DIR){
   gene_name <- (as.character(rownames(select_genecount)))
   select_genecount <- as.data.frame(cbind(gene_name,select_genecount))
   rownames(select_genecount) <- NULL
-  intersect_gene <- intersect(select_genecount$gene_name, decay_methy$gene_name)
+  intersect_gene <- intersect(select_genecount$gene_name, gene_methy_infor$gene_name)
   match_data <- data.frame()
   for (i in 1:length(intersect_gene)) {
 
     methy_name <- intersect_gene[i]
     match_gene <- as.data.frame(select_genecount[which(!is.na(match(select_genecount$gene_name,  methy_name))),])
-    select_methy <- as.data.frame(decay_methy[which(!is.na(match(decay_methy$gene_name, methy_name))),])
+    select_methy <- as.data.frame(gene_methy_infor[which(!is.na(match(gene_methy_infor$gene_name, methy_name))),])
     match_methy_expr <- cbind(match_gene, select_methy[,-1])
     match_data <- rbind(match_data, match_methy_expr)
 
+  }
+  if(is.na(OUTPUT_DIR)){
+    OUTPUT_DIR=getwd()
   }
   dir.create(paste0(OUTPUT_DIR,"/m6Aexpress_result"))
   write.table(match_data, file = paste0(OUTPUT_DIR,"/m6Aexpress_result/","expr_methy.tab"), quote = FALSE, row.names = FALSE)
