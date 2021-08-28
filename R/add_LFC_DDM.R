419ad6a64db9ce128ec76d350af7bda2cae454a8
NWPU-903PR <37072138+NWPU-903PR@users.noreply.github.com>
2021-08-28 22:21:26 +0800

    Update add_LFC_DDM.R

--- R/add_LFC_DDM.R
+++ R/add_LFC_DDM.R
@@ -1,5 +1,7 @@
 add_LFC_DDM <- function(expre_methyre, DE_gene, methy_intensity,num_cond1,OUTPUT_DIR=NA){
   DE_infor <- DE_gene[[3]]
+  DE_infor <- data.frame(gene_name=as.character(rownames(DE_infor)),DE_infor)
+  rownames(DE_infor) <- NULL
   control_methyintensity <- as.data.frame(methy_intensity[,2:(num_cond1+1)]) 
   treated_mehtyintensity <- as.data.frame(methy_intensity[,-c(1,2:(num_cond1+1))])
   DDM <- rowMeans(treated_mehtyintensity)-rowMeans(control_methyintensity)
@@ -7,8 +9,8 @@ add_LFC_DDM <- function(expre_methyre, DE_gene, methy_intensity,num_cond1,OUTPUT
   m6A_reg_expr_gene <- as.character(expre_methyre$gene_name)
   m6Aexpree_gene_DDMLFC <- data.frame()
   for (i in 1:length(m6A_reg_expr_gene)) {
-    one_DEDM <- as.data.frame(cbind(expre_methyre[i,],DE_infor[DE_infor$gene_name==m6A_reg_expr_gene[i],]$log2foldchange,
-                              add_methyDDM[add_methyDDM$gene_name==m6A_reg_expr_gene[i,ncol(add_methyDDM)]]))
+    one_DEDM <- as.data.frame(cbind(expre_methyre[i,],DE_infor[DE_infor$gene_name==m6A_reg_expr_gene[i],]$log2FoldChange,
+                                    add_methyDDM[add_methyDDM$gene_name==m6A_reg_expr_gene[i],ncol(add_methyDDM)]))
     colnames(one_DEDM)[(ncol(one_DEDM)-1):ncol(one_DEDM)] <- c("DE_log2foldchange","DM_intensity")
     m6Aexpree_gene_DDMLFC <- rbind(m6Aexpree_gene_DDMLFC,one_DEDM)
   }
@@ -18,6 +20,6 @@ add_LFC_DDM <- function(expre_methyre, DE_gene, methy_intensity,num_cond1,OUTPUT
     write.table(m6Aexpree_gene_DDMLFC,file=paste(OUTPUT_DIR,"m6Aexpress_result", "m6A_reg_exp_LFC_DDM.xls",sep="/"), sep="\t",row.names =FALSE,quote = FALSE)
   }
   if(!is.na(OUTPUT_DIR)){
-     write.table(m6Aexpree_gene_DDMLFC,file=paste(OUTPUT_DIR,sep="/", "m6A_reg_exp_LFC_DDM.xls"), sep="\t",row.names =FALSE,quote = FALSE)
+    write.table(m6Aexpree_gene_DDMLFC,file=paste(OUTPUT_DIR,sep="/", "m6A_reg_exp_LFC_DDM.xls"), sep="\t",row.names =FALSE,quote = FALSE)
   }
 }
